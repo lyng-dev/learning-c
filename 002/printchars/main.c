0fe58865a5932173e986a4f1df10cb527b56551f
Steffen Sun Lyng <steffen.lyng@jp.dk>
2020-03-07 19:01:02 +0100

    changed printchars to use unsigned int

---
+++ 002/printchars/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+
+  unsigned int ctr;
+
+  for(ctr = 65; ctr < 91; ctr++) {
+    printf("%c", ctr);
+  }
+
+  printf("\n");
+
+  return 0;
+}
