ad55c2a9ff44f4d85129d55fa524a7e5cada3be1
Steffen Sun Lyng <steffen.lyng@jp.dk>
2020-03-07 23:10:43 +0100

    Added explicit type program

---
+++ 003/explicittype/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+typedef short human_age;
+
+int main(void) {
+
+  human_age theDevelopersAge = 40;
+
+  printf("The age of the developer is: %d", theDevelopersAge);
+
+  return 0;
+
+}
