cb0d1e4f0159bab0649ee648ccb710b40d7eb986
Steffen Sun Lyng <steffen.lyng@jp.dk>
2020-03-07 23:22:33 +0100

    Added symbolic constant program

---
+++ 003/symbolicconst/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#define THE_ANSWER 42
+
+int main(void) {
+
+  printf("The answer to the ultimate question of life, the universe and everything ... is.... %d", THE_ANSWER);
+
+}
