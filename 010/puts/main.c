5da625c9919b088e19d2c298b14c28331a7c8e29
Steffen Sun Lyng <steffen.lyng@jp.dk>
2020-03-14 02:57:05 +0100

    Added puts program to print stringss

---
+++ 010/puts/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+char *m1 = "C";
+char *m2 = "Is";
+char *m3 = "The best";
+char *m4 = "programming";
+char *m5 = "language!";
+
+int main() {
+
+  puts(m1);
+  puts(m2);
+  puts(m3);
+  puts(m4);
+  puts(m5);
+
+  return 0;
+}
