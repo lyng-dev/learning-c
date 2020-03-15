1651e1ebafef90a7753d3ab535439de8017264fe
Steffen Sun Lyng <steffen.lyng@jp.dk>
2020-03-15 01:15:12 +0100

    Added switch statement with fallthru program

---
+++ 014/fallthru/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+  int reply;
+
+  while(1) {
+    puts("\nEnter a value between 1 and 10, 0 to exit: ");
+    scanf("%d", &reply);
+
+    switch (reply) {
+      case 0:
+        exit(0);
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5: {
+        puts("You entered 5 or below!\n");
+        break;
+      }
+      case 6:
+      case 7:
+      case 8:
+      case 9:
+      case 10: {
+        puts("You entered 6 or higher\n");
+        break;
+      }
+      default:
+        puts("Nono, between 1 and 10 please!\n");
+    }
+  }
+
+  return 0;
+}
